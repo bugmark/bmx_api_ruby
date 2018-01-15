e2f53177546632be81e9a5650f71988fd1827436
andyl <andy@r210.com>
2018-01-15 05:51:54 -0800

    Add thor_base

--- lib/cmd/contract.rb
+++ lib/cmd/contract.rb
@@ -1,13 +1,30 @@
 require 'thor'
 
 class Contract < Thor
-  desc "one", "one"
-  def one
-    puts "one"
+  desc "list", "List all open contracts"
+  long_desc <<~EOF
+    Return summary information for all open contracts.
+  EOF
+  def list
+    under_construction
   end
 
-  desc "two", "two"
-  def two
-    puts "two"
+  desc "show CONTRACT_UUID", "Show contract details"
+  long_desc <<~EOF
+    Show all information for a single contract.
+  EOF
+  def show(_uuid)
+    under_construction
+  end
+
+  desc "cancel CONTRACT_UUID", "Cancel contract"
+  long_desc <<~EOF
+    Cancel a single contract.
+
+    Note: this is only possible for contracts that are OPEN and have
+    no attached Escrows.
+  EOF
+  def cancel(_uuid)
+    under_construction
   end
 end
